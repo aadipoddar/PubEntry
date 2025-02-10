283fa09828abc6a84cefca70131cdae91d4482ad
Aadi Poddar <aadipoddarmail@gmail.com>
2025-02-10 20:25:49 +0530

    Remove Secrets

--- PubEntryLibrary/DataAccess/Secrets.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PubEntryLibrary.DataAccess;
-
-public static class Secrets
-{
-	public static string DatabaseUserId => "";
-	public static string DatabaseName => "";
-	public static string DatabasePassword => "";
-	public static string SyncfusionLicense => "";
-}
