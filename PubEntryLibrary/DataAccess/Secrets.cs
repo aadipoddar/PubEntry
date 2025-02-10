c01e6e21b06ed5e0e932001b39ba50d6014fd944
Aadi Poddar <aadipoddarmail@gmail.com>
2025-02-10 20:17:11 +0530

    Upload Secrets

---
+++ PubEntryLibrary/DataAccess/Secrets.cs
@@ -0,0 +1,9 @@
+﻿namespace PubEntryLibrary.DataAccess;
+
+public static class Secrets
+{
+	public static string DatabaseUserId => "";
+	public static string DatabaseName => "";
+	public static string DatabasePassword => "";
+	public static string SyncfusionLicense => "";
+}
