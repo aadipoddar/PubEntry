1f81e378730194c682ec2b1800ec5e3d167337b3
Aadi Poddar <aadipoddarmail@gmail.com>
2025-02-23 21:12:12 +0530

    WPF Project Transaction and Dashboard UI

---
+++ PubEntryWPF/Dashboard.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System.Reflection;
+using System.Windows;
+
+namespace PubEntryWPF;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class Dashboard : Window
+{
+	public Dashboard()
+	{
+		InitializeComponent();
+		UpdateCheck();
+	}
+
+	private static async void UpdateCheck()
+	{
+		var isUpdateAvailable = await AadiSoftUpdater.AadiSoftUpdater.CheckForUpdates("aadipoddar", $"{Secrets.DatabaseName}", Assembly.GetExecutingAssembly().GetName().Version.ToString());
+
+		if (!isUpdateAvailable) return;
+		if (MessageBox.Show("New Version Available. Do you want to update?", "Update Available", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+			await AadiSoftUpdater.AadiSoftUpdater.UpdateApp("aadipoddar", $"{Secrets.DatabaseName}", "PubEntrySetup", "477557B4-2908-4106-B360-D2D114F02452");
+	}
+
+	private async void Window_Loaded(object sender, RoutedEventArgs e) => await LoadLocationComboBox();
+
+	private async Task LoadLocationComboBox()
+	{
+		locationComboBox.ItemsSource = await CommonData.LoadTableDataByStatus<LocationModel>(Table.Location);
+		locationComboBox.DisplayMemberPath = nameof(LocationModel.Name);
+		locationComboBox.SelectedValuePath = nameof(LocationModel.Id);
+		locationComboBox.SelectedIndex = 0;
+
+		richTextBoxFooter.Text = $"Version: {Assembly.GetExecutingAssembly().GetName().Version}";
+	}
+
+	private async Task LoadUserComboBox()
+	{
+		userComboBox.ItemsSource = await UserData.LoadUsersByLocationId((locationComboBox.SelectedItem as LocationModel).Id);
+		userComboBox.DisplayMemberPath = nameof(UserModel.Name);
+		userComboBox.SelectedValuePath = nameof(UserModel.Id);
+		userComboBox.SelectedIndex = 0;
+	}
+
+	private async void locationComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e) => await LoadUserComboBox();
+
+	private void userComboBox_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e) => passwordBox.Clear();
+
+	private bool ValidatePassword()
+	{
+		if ((userComboBox.SelectedItem as UserModel)?.Password == passwordBox.Password)
+		{
+			passwordBox.Clear();
+			return true;
+		}
+
+		passwordBox.Clear();
+		return false;
+	}
+
+	private void entryButton_Click(object sender, RoutedEventArgs e)
+	{
+		if (!ValidatePassword())
+		{
+			MessageBox.Show("Incorrect Password", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+			return;
+		}
+
+		Transaction.Transaction transaction = new(this);
+		Hide();
+		transaction.ShowDialog();
+	}
+
+	private void advanceButton_Click(object sender, RoutedEventArgs e)
+	{
+
+	}
+
+	private void reportsButton_Click(object sender, RoutedEventArgs e)
+	{
+
+	}
+
+	private void adminButton_Click(object sender, RoutedEventArgs e)
+	{
+
+	}
+}
---
+++ PubEntryWPF/Transaction/Transaction.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Reflection;
+using System.Text.RegularExpressions;
+using System.Windows;
+
+namespace PubEntryWPF.Transaction
+{
+	/// <summary>
+	/// Interaction logic for Transaction.xaml
+	/// </summary>
+	public partial class Transaction : Window
+	{
+		private readonly Dashboard _dashboard;
+
+		public Transaction(Dashboard dashboard)
+		{
+			InitializeComponent();
+			_dashboard = dashboard;
+		}
+
+		private async void Window_Loaded(object sender, RoutedEventArgs e)
+		{
+			reservationComboBox.ItemsSource = await CommonData.LoadTableData<ReservationTypeModel>(Table.ReservationType);
+			reservationComboBox.DisplayMemberPath = nameof(ReservationTypeModel.Name);
+			reservationComboBox.SelectedValuePath = nameof(ReservationTypeModel.Id);
+			reservationComboBox.SelectedIndex = 0;
+
+			richTextBoxFooter.Text = $"Version: {Assembly.GetExecutingAssembly().GetName().Version}";
+		}
+
+		private void Window_Closed(object sender, EventArgs e)
+		{
+			Close();
+			_dashboard.Show();
+		}
+
+		private void maleTextBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+		{
+
+		}
+
+		private void textBox_PreviewTextInput(object sender, System.Windows.Input.TextCompositionEventArgs e)
+		{
+			Regex regex = new("[^0-9]+");
+			e.Handled = regex.IsMatch(e.Text);
+		}
+	}
+}
