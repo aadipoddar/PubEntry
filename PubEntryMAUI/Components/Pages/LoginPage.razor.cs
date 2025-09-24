25dfc2b3c65577f6f7658b46449b48b29038c507
Aadi Poddar <aadipoddarmail@gmail.com>
2025-09-24 14:30:22 +0530

    Added Location Combo box

--- PubEntryMAUI/Components/Pages/LoginPage.razor.cs
+++ PubEntryMAUI/Components/Pages/LoginPage.razor.cs
@@ -4,15 +4,19 @@ using PubEntryLibrary.Data;
 using PubEntryLibrary.DataAccess;
 using PubEntryLibrary.Models;
 
+using Syncfusion.Blazor.DropDowns;
+
 namespace PubEntryMAUI.Components.Pages;
 
 public partial class LoginPage
 {
 	[Inject] private NavigationManager NavManager { get; set; }
 
+	private int _selectedLocationId;
 	private int _selectedUserId;
 	private string _password = string.Empty;
 
+	private List<LocationModel> _locations = [];
 	private List<UserModel> _users = [];
 
 	protected override async Task OnInitializedAsync()
@@ -23,7 +27,36 @@ public partial class LoginPage
 
 	private async Task LoadData()
 	{
-		_users = await CommonData.LoadTableDataByStatus<UserModel>(TableNames.User);
+		_locations = await CommonData.LoadTableDataByStatus<LocationModel>(TableNames.Location);
+		_selectedLocationId = _locations.FirstOrDefault()?.Id ?? 0;
+
+		_users = await UserData.LoadUsersByLocationId(_selectedLocationId);
+		_selectedUserId = _users.FirstOrDefault()?.Id ?? 0;
+
+		_password = string.Empty;
+
+		StateHasChanged();
+	}
+
+	private async Task OnLocationChanged(ChangeEventArgs<int, LocationModel> e)
+	{
+		if (e?.Value is null || e?.Value == 0)
+			_selectedLocationId = _locations.FirstOrDefault()?.Id ?? 0;
+
+		else
+			_selectedLocationId = e.Value;
+
+		List<UserModel> admins = [];
+
+		foreach (var location in _locations)
+			admins.AddRange((await UserData.LoadUsersByLocationId((location as LocationModel).Id)).ToList().Where(_ => _.Admin));
+
+		_users = [.. admins
+				.Concat((await UserData.LoadUsersByLocationId(_selectedLocationId))
+				.ToList()
+				.Where(_ => !_.Admin))
+				.OrderBy(_ => _.Admin)
+				.OrderBy(_ => _.Name)];
 		_selectedUserId = _users.FirstOrDefault()?.Id ?? 0;
 
 		_password = string.Empty;
@@ -33,7 +66,7 @@ public partial class LoginPage
 
 	private async Task LogIn()
 	{
-		if (_selectedUserId == 0 || string.IsNullOrWhiteSpace(_password))
+		if (_selectedUserId == 0 || _selectedLocationId == 0 || string.IsNullOrWhiteSpace(_password))
 		{
 			await Application.Current.MainPage.DisplayAlert("Error", "Please select a user, location and enter a password.", "OK");
 			return;
